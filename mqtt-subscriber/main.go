ed3c08eca9bcc6e6c1ba773fa8d3503397dbec02
AndresGR02 <andres.gr.workspace@gmail.com>
2024-04-28 20:15:39 -0600

    Create mqtt subscriber
    Created a connection between the broker and the backend through tls and mqtt

--- mqtt-subscriber/main.go
+++ mqtt-subscriber/main.go
@@ -1,91 +1,43 @@
 package main
 
 import (
-	_ "github.com/microsoft/go-mssqldb"
-	"database/sql"
-	"context"
-	"log"
 	"fmt"
-	"time"
-    "os"
-    "github.com/joho/godotenv"
-)
+	"log"
+	"os"
 
-var db *sql.DB
-var server = ""
-var port = 1433
-var user = ""
-var password = ""
-var database = "test"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
-	var err error
-    
-    err = godotenv.Load(".env")
+    err := godotenv.Load(".env")
     if err != nil {
-        log.Fatalf("Error loading environment variables file")
+        log.Fatalf("Error loading .env file: %s", err)
+    }
+
+
+    var broker = os.Getenv("MQTT_URL")
+    var port = 8883
+    opts := mqtt.NewClientOptions()
+    opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
+    opts.SetUsername(os.Getenv("MQTT_USERNAME"))
+    opts.SetPassword(os.Getenv("MQTT_KEY"))
+
+    client := mqtt.NewClient(opts)
+    
+    if token := client.Connect(); token.Wait() && token.Error() != nil {
+        panic(token.Error())
     }
     
-    user = os.Getenv("DATABASE")
-    password = os.Getenv("KEY")
-    server = os.Getenv("DB_ENDPOINT")
+    topic := "home"
 
-    // Build connection string
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-		server, user, password, port, database)
+    if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+        fmt.Println(token.Error())
+    }
 
-	// Create connection pool
-	db, err = sql.Open("sqlserver", connString)
-	if err != nil {
-		log.Fatal("Error creating connection pool: ", err.Error())
-	}
-	defer db.Close() // Close the connection when main exits
+    client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+        fmt.Printf("Recieved message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+    })
 
-	ctx := context.Background()
-	err = db.PingContext(ctx)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	fmt.Println("Connected!")
-
-	count, err := ReadBuildVersion()
-	if err != nil {
-		log.Fatal("Error reading build version: ", err.Error())
-	}
-
-	fmt.Printf("Read %d row(s) successfully.\n", count)
-}
-
-func ReadBuildVersion() (int, error) {
-	ctx := context.Background()
-
-	err := db.PingContext(ctx)
-	if err != nil {
-		return -1, err
-	}
-
-	tsql := "SELECT TOP 1 VersionDate FROM dbo.BuildVersion;"
-
-	rows, err := db.QueryContext(ctx, tsql)
-	if err != nil {
-		return -1, err
-	}
-	defer rows.Close()
-
-	var count int
-
-	for rows.Next() {
-		var versionDate time.Time
-		err := rows.Scan(&versionDate)
-		if err != nil {
-			return -1, err
-		}
-
-        fmt.Printf("Version Date: %s\n", versionDate.Format("2006-01-02 15:04:05"))
-		count++
-	}
-
-	return count, nil
 }
