24ff7c76d708793f9ae8965db3a2a6a9b0fe33d2
AndresGR04 <andres.gr.workspace@gmail.com>
2024-04-29 20:46:01 -0600

    Update mqtt-subscriber
    Now the mqtt subscriber is correctly recieving messages from the subscribed topic

--- mqtt-subscriber/main.go
+++ mqtt-subscriber/main.go
@@ -4,40 +4,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error loading .env file: %s", err)
+	}
 
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatalf("Error loading .env file: %s", err)
-    }
+	// MQTT connection options
+	opts := mqtt.NewClientOptions().
+		AddBroker(os.Getenv("MQTT_URL")).
+		SetUsername(os.Getenv("MQTT_USERNAME")).
+		SetPassword(os.Getenv("MQTT_KEY"))
 
+	// Create MQTT client
+	client := mqtt.NewClient(opts)
 
-    var broker = os.Getenv("MQTT_URL")
-    var port = 8883
-    opts := mqtt.NewClientOptions()
-    opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
-    opts.SetUsername(os.Getenv("MQTT_USERNAME"))
-    opts.SetPassword(os.Getenv("MQTT_KEY"))
+	// Connect to the MQTT broker
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Error connecting to MQTT broker: %s", token.Error())
+	}
+	defer client.Disconnect(250)
 
-    client := mqtt.NewClient(opts)
-    
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
-        panic(token.Error())
-    }
-    
-    topic := "home"
+	// Subscribe to a topic
+	topic := "home"
+	if token := client.Subscribe(topic, 0, messageHandler); token.Wait() && token.Error() != nil {
+		log.Fatalf("Error subscribing to topic %s: %s", topic, token.Error())
+	}
 
-    if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
-        fmt.Println(token.Error())
-    }
+	// Handle graceful shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 
-    client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
-        fmt.Printf("Recieved message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-    })
+	// Keep listening for messages until a termination signal is received
+	for {
+		select {
+		case <-signalChan:
+			log.Println("Shutting down...")
+			return // Exit the loop and terminate the program
+		}
+	}
+}
 
+// messageHandler is the callback function for incoming messages
+func messageHandler(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 }
